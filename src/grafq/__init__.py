df039056dc3dc009de3b9b6a1edfd6de9935f86d
André Sá de Mello <asmello.br@gmail.com>
2021-10-24 16:15:20 +0100

    exports refactor

--- src/grafq/__init__.py
+++ src/grafq/__init__.py
@@ -1,2 +1,10 @@
 from .blueprints import QueryBlueprint as Query, FieldBlueprint as Field
-from .language import VarRef as Var, Null, NamedType, ListType, NonNullType
+from .language import (
+    VarRef as Var,
+    Null,
+    NullType,
+    NamedType,
+    ListType,
+    NonNullType,
+    ScalarExtension,
+)
